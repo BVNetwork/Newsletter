05614a94ea7dbbfb25cdaaff3351c37f30524295
Haugsten <haakon@episerver.no>
2019-11-14 12:46:42 +0100

    Upgrade to SendGrid 9.12.0

--- src/Newsletter.SendGrid/MailSenderSendGrid.cs
+++ src/Newsletter.SendGrid/MailSenderSendGrid.cs
@@ -1,26 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
-using System.Net;
-using System.Net.Mail;
+using BVNetwork.EPiSendMail.Configuration;
 using BVNetwork.EPiSendMail.DataAccess;
 using BVNetwork.EPiSendMail.Library;
 using SendGrid;
+using SendGrid.Helpers.Mail;
+using EmailAddress = SendGrid.Helpers.Mail.EmailAddress;
 
-// ReSharper disable PossibleMultipleEnumeration
 
 namespace BVNetwork.EPiSendMail.SendGrid
 {
     public class MailSenderSendGrid : MailSenderBatchBase, IMailSenderVerification
     {
-        private const string ConnectionStringName = "EPiCode.Newsletter.SendGrid";
-
+        private const string ApiKeyName = "Newsletter.SendGrid.ApiKey";
 
         public class SendGridSettings
         {
-            public string Username { get; set; }
-            public string Password { get; set; }
+            public string ApiKey { get; set; }
         }
 
         /// <summary>
@@ -36,45 +33,55 @@ namespace BVNetwork.EPiSendMail.SendGrid
             var settings = GetSettings();
 
             if (recipients == null || recipients.Any() == false)
-                throw new ArgumentException("No workitems", "recipients");
+                throw new ArgumentException("No workitems", nameof(recipients));
 
             if (recipients.Count() > 1000)
-                throw new ArgumentOutOfRangeException("recipients", "SendGrid supports maximum 1000 recipients per batch send.");
+                throw new ArgumentOutOfRangeException(nameof(recipients), "SendGrid supports maximum 1000 recipients per batch send.");
 
-            var msg = new SendGridMessage();
-            msg.From = new MailAddress(mail.From);
-            msg.Subject = mail.Subject;
-            msg.Html = mail.BodyHtml;
-            msg.Text = mail.BodyText;
-            
-            // Add recipinets to header, to hide other recipients in to field. 
-            List<string> addresses = recipients.Select(r => r.EmailAddress).ToList();
-            msg.Header.SetTo(addresses);
-            msg.AddSubstitution("%recipient%", addresses);
-            // To send message we need to have a to address, set that to from
-            msg.To = new MailAddress[] { msg.From };
+            var msg = new SendGridMessage
+            {
+                From = new EmailAddress(mail.From),
+                Subject = mail.Subject,
+                HtmlContent = mail.BodyHtml,
+                PlainTextContent = mail.BodyText
+            };
+
+            var personalizations = new List<Personalization>();
+            foreach (var jobWorkItem in recipients)
+            {
+                var personalization = new Personalization();
+                personalization.Tos = new List<EmailAddress>
+                {
+                    new EmailAddress(jobWorkItem.EmailAddress)
+                };
+                personalization.Substitutions = new Dictionary<string, string>
+                {
+                    {"%recipient%", jobWorkItem.EmailAddress}
+                };
+
+                personalizations.Add(personalization);  
+            }
+
+            msg.Personalizations = personalizations;
 
             if (mail.EnableTracking)
             {
                 // true indicates that links in plain text portions of the email 
                 // should also be overwritten for link tracking purposes. 
-                msg.EnableClickTracking(true);
-                msg.EnableOpenTracking();
+                msg.SetClickTracking(true,true);
+                msg.SetOpenTracking(true);
             }
 
             if(mail.CustomProperties.ContainsKey("SendGridCategory"))
             {
                 string category = mail.CustomProperties["SendGridCategory"] as string;
                 if (string.IsNullOrEmpty(category) == false)
-                    msg.SetCategory(category);
+                    msg.Categories.Add(category);
             }
+            
+            var client = new SendGridClient(settings.ApiKey);
 
-            var credentials = new NetworkCredential(settings.Username, settings.Password);
-
-            // Create an Web transport for sending email.
-            var transportWeb = new Web(credentials);
-
-            transportWeb.Deliver(msg);
+            client.SendEmailAsync(msg).Wait();
 
             return true;
         }
@@ -84,14 +91,9 @@ namespace BVNetwork.EPiSendMail.SendGrid
             EnvironmentVerification env = new EnvironmentVerification();
 
             SendGridSettings settings = GetSettings();
-            if (string.IsNullOrEmpty(settings.Username))
+            if (string.IsNullOrEmpty(settings.ApiKey))
             {
-                env.VerificationItems.Add(VerificationType.Error, "Missing username setting in SendGrid connection string.");
-            }
-
-            if (string.IsNullOrEmpty(settings.Password))
-            {
-                env.VerificationItems.Add(VerificationType.Error, "Missing password setting in SendGrid connection string.");
+                env.VerificationItems.Add(VerificationType.Error, "Missing api key setting for SendGrid.");
             }
 
             return env;
@@ -99,30 +101,10 @@ namespace BVNetwork.EPiSendMail.SendGrid
 
         protected SendGridSettings GetSettings()
         {
-            SendGridSettings settings = new SendGridSettings();
-
-            ConnectionStringSettings connectionString = ConfigurationManager.ConnectionStrings[ConnectionStringName];
-            if(connectionString == null)
-                return settings;
-            
-            var keyValuePairs = connectionString.ConnectionString.Split(new[]{';'}, StringSplitOptions.RemoveEmptyEntries)
-                                        .Where(kvp => kvp.Contains('='))
-                                        .Select(kvp => kvp.Split(new char[] { '=' }, 2))
-                                        .ToDictionary(kvp => kvp[0].Trim(),
-                                                      kvp => kvp[1].Trim(),
-                                                      StringComparer.InvariantCultureIgnoreCase);
- 
-            
-            
-            // Mandatory
-            if (keyValuePairs.ContainsKey("username"))
-                settings.Username = keyValuePairs["username"];
-            if (keyValuePairs.ContainsKey("password"))
-                settings.Password = keyValuePairs["password"];
-            
-            // Optional
-
-            return settings;
+            return new SendGridSettings
+            {
+                ApiKey = NewsLetterConfiguration.GetAppSettingsConfigValueEx<string>("Newsletter.SendGrid.ApiKey", "")
+            };
         }
 
         public override int GetBatchSize()
@@ -130,7 +112,5 @@ namespace BVNetwork.EPiSendMail.SendGrid
             // SendGrid lets us send 1000 each time. We'll default to 999
             return 999;
         }
-
-
     }
 }
